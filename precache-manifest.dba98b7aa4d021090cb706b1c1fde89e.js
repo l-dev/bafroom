12838527d46f041f778df73bbd9a18838c665bcc
lauren robinson <laurenrobinson@laurens-MacBook-Air.local>
2019-11-19 09:07:46 -0800

    Updates

---
+++ precache-manifest.dba98b7aa4d021090cb706b1c1fde89e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161f8d7a74c81f066ec7b876e53da698",
+    "url": "/bafroom/index.html"
+  },
+  {
+    "revision": "65f40f830a4137e28541",
+    "url": "/bafroom/static/css/main.f80286e6.chunk.css"
+  },
+  {
+    "revision": "31851a7aee020a0fafbb",
+    "url": "/bafroom/static/js/2.df6a762e.chunk.js"
+  },
+  {
+    "revision": "65f40f830a4137e28541",
+    "url": "/bafroom/static/js/main.1eb46b73.chunk.js"
+  },
+  {
+    "revision": "eca2aeb079efdb257315",
+    "url": "/bafroom/static/js/runtime-main.833fdcea.js"
+  }
+]);
